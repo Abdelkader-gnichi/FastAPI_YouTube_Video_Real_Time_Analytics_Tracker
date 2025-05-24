9a058380576ef5cfe3db64978fda420d233bed1a
abdelkader gnichi <ggnichi097@gmail.com>
2025-05-24 10:52:37 +0100

    init main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,36 @@
-def main():
-    print("Hello from backend!")
+from contextlib import asynccontextmanager
+from typing import Union
+# from api.db.session import init_db
+from fastapi import FastAPI
+# from api.events import router as events_router
+from fastapi.middleware.cors import CORSMiddleware
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # before startup
+    # init_db()
+    yield
+    # clean up
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# app.include_router(events_router, prefix='/api/events')
+
+@app.get('/')
+def greeting():
+    return {'message': 'hello world!'}
+
+@app.get('/healthz')
+def get_api_health_status():
+    return {'status': 'ok'}
+
+
+
